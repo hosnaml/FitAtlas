89692152b11e6f39097144fabc8f5482d48fd477
hosna <hosna@kth.se>
2025-07-14 16:27:41 +0200

    Update ExerciseCard on Search

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -7,14 +7,29 @@ import ExerciseCard from './ExerciseCard'
 
 function Exercises({exercises, setExercises, bodyPart}) {
  
+  const [currentPage, setCurrentPage] = useState(1)
+  const exercisesPerPage = 9
+  const indexOfLastExercise = currentPage * exercisesPerPage
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise)
   
+  const paginate = (e, value) => {
+    setCurrentPage(value)
+    window.scrollTo({top: 1800, left: 100, behavior: 'smooth'})
+  }
+
   return (
     <Box id="exercises" sx={{mt: {lg: '110px'}}} mt="50px" p="20px">
       <Typography variant="h3" mb="46px" >Showing Results</Typography>
       <Stack direction="row" sx={{gap: {lg: '110px', xs: '50px'}}} flexWrap="wrap" justifyContent="center">
-          {exercises.map((exercise, index) => (
+          {currentExercises.map((exercise, index) => (
             <ExerciseCard key={index} exercise={exercise}/>
           ))}
+      </Stack>
+      <Stack mt="100px" alignItems="center" width="100%">
+        {exercises.length > 9 && (
+          <Pagination color="standard" shape="rounded" defaultPage={1} count={Math.ceil(exercises.length / 9)} page={page} onChange={paginate} size="large" />
+        )}
       </Stack>
     </Box>
   )
